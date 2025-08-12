ee40ba3056e0059c1dc410eaf44f7eefdcce964b
rodrigouab2025 <rodrigo.chanez@uab.edu.bo>
2025-08-11 20:15:02 -0400

    api login

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'min:8'],
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Credenciales incorrectas.'], 401);
+        }
+        if ($user->estado !== 'S') {
+            return response()->json([
+                'message' => 'Tu cuenta está inactiva, contacta al administrador.'
+            ], 403);
+        }
+        $user->tokens()->delete();
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login exitoso.',
+            'token' => $token,
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'rol' => $user->rol,
+                'telefono' => $user->telefono,
+            ]
+        ]);
+    }
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'      => 'required|string|max:255',
+            'email'     => 'required|string|email|max:255|unique:users',
+            'password'  => 'required|string|min:8|confirmed',
+            'telefono'  => 'nullable|digits:8',
+            'rol'       => 'nullable|in:paciente,medico,administrador',
+        ]);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+            'telefono' => $request->telefono,
+            'rol'      => $request->rol ?? 'paciente'
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'Usuario registrado correctamente.',
+            'data'    => [
+                'user'  => $user,
+                'token' => $token
+            ]
+        ], 201);
+    }
+
+}
